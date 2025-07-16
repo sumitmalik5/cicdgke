c145f44a1ebdbeb335a66fc74a6bee4406e38370
Sumit Malik <89841037+sumitmalik5@users.noreply.github.com>
2025-07-16 13:05:50 +1000

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Welcome to Python Flask World v5.0 from cicd-gke'
+    return 'Welcome to Python Flask World v6.0 from cicd-gke'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
